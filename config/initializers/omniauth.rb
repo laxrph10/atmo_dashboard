1db830b9b3c896efa3567309c30d5aebb987af7d
jasperknowsbest <edward.jasper@flatironschool.com>
2014-08-20 13:40:23 -0400

    add gems

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :instagram, ENV['f924b30e8ea849618d110275308306f3'], ENV['1b50bc12ff8d4571b2adb930f127cc6a'],
-  provider :soundcloud, ENV['527aa007668d6c7a500d574126ee8904'], ENV['c16493ab78bf5e601de8b6dc8f2216f3'],
+  provider :instagram, ENV['f924b30e8ea849618d110275308306f3'], ENV['1b50bc12ff8d4571b2adb930f127cc6a']
+  provider :soundcloud, ENV['527aa007668d6c7a500d574126ee8904'], ENV['c16493ab78bf5e601de8b6dc8f2216f3']
   provider :vimeo, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
